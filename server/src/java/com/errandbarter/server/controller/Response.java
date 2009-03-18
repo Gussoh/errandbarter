768284afdd89a30278e0e48b129ce6aa91d69969
zachlimrj <zachlimrj@2f0039ac-0a15-11de-98bb-07cc9828235c>
2009-03-18 17:01:39 +0000

    controller package included with some servlets

--- server/src/java/com/errandbarter/server/controller/Response.java
+++ server/src/java/com/errandbarter/server/controller/Response.java
@@ -21,19 +21,24 @@ public class Response {
     /** Creates a new instance of Response */
     public Response() {
     }
-
+    
+    public Response(String message, String status) {
+        this.message = message;
+        this.status = status;
+    }
+    
     public String getMessage() {
         return message;
     }
-
+    
     public void setMessage(String message) {
         this.message = message;
     }
-
+    
     public String getStatus() {
         return status;
     }
-
+    
     public void setStatus(String status) {
         this.status = status;
     }
--- server/src/java/com/errandbarter/server/xml/SimpleXMLWriter.java
+++ server/src/java/com/errandbarter/server/xml/SimpleXMLWriter.java
@@ -10,6 +10,7 @@
 package com.errandbarter.server.xml;
 
 import com.errandbarter.server.controller.Response;
+import com.errandbarter.server.dao.mock.MockDAOFactory;
 import com.errandbarter.server.entity.Errand;
 import com.errandbarter.server.entity.User;
 import java.util.List;
@@ -19,21 +20,29 @@ import java.util.List;
  * @author Zach
  */
 public class SimpleXMLWriter implements XMLWriter {
-
+    
+    private final static SimpleXMLWriter singleton = new SimpleXMLWriter();
+    
+    protected SimpleXMLWriter() {}
+    
+    public static SimpleXMLWriter getInstance() {
+        return singleton;
+    }
+    
     public String getXML(List<Errand> Errands) {
         return null;
     }
-
+    
     public String getXML(Errand errand) {
         return null;
     }
-
+    
     public String getXML(User user) {
         return null;
     }
-
+    
     public String getXML(Response response) {
         return null;
     }
-       
+    
 }
