eb72bf89d93de98a32fbba754e3e1a55bbb07fde
zachlimrj <zachlimrj@2f0039ac-0a15-11de-98bb-07cc9828235c>
2009-03-18 16:38:13 +0000

    mockfactory

--- server/src/java/com/errandbarter/server/dao/mock/MockDAOFactory.java
+++ server/src/java/com/errandbarter/server/dao/mock/MockDAOFactory.java
@@ -5,16 +5,54 @@
 
 package com.errandbarter.server.dao.mock;
 
-import java.util.List;
+import com.errandbarter.server.dao.AnswerDAO;
+import com.errandbarter.server.dao.DAOFactory;
+import com.errandbarter.server.dao.ErrandDAO;
+import com.errandbarter.server.dao.UserDAO;
+import com.errandbarter.server.entity.Answer;
+import com.errandbarter.server.entity.Errand;
+import com.errandbarter.server.entity.User;
+import java.util.Hashtable;
+import java.util.Map;
 
 /**
  *
  * @author Zach
  */
-public interface MockDAOFactory {
-
-   public ErrandDAO getErrandDAO();
-   public UserDAO getUserDAO();
-   public AnswerDAO getAnswerDAO();
+public class MockDAOFactory implements DAOFactory {
+    
+    private final static MockDAOFactory INSTANCE = new MockDAOFactory();
+    
+    private ErrandDAO errandDAO;
+    private UserDAO userDAO;
+    private AnswerDAO answerDAO;
+    
+    protected MockDAOFactory() {
+        //init mock DAOs with mock data and populate data (TODO)
+        Map errandDB = new Hashtable<Integer, Errand>();
+        errandDAO = new MockErrandDAO(errandDB);
+        
+        Map userDB = new Hashtable<String, User>();
+        userDAO = new MockUserDAO(userDB);
+        
+        Map answerDB = new Hashtable<Integer, Answer>();
+        answerDAO = new MockAnswerDAO(answerDB);
+    }
+    
+    public static MockDAOFactory getInstance() {
+        return INSTANCE;
+    }
+    
+    public ErrandDAO getErrandDAO() {
+        return errandDAO;
+    }
+    
+    public UserDAO getUserDAO() {
+        return userDAO;
+    }
+    
+    public AnswerDAO getAnswerDAO() {
+        return answerDAO;
+    }
 
 }
